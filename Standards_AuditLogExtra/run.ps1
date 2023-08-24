d067db1db444176e7918e725f000a90328cfe4b7
jrandRTCC <66025354+jrandRTCC@users.noreply.github.com>
2023-08-24 13:49:05 -0400

    Add files via upload

---
+++ Standards_AuditLogExtra/run.ps1
@@ -0,0 +1,19 @@
+param($tenant)
+
+Write-LogMessage -API "Standards" -tenant $tenant -message "Trying to apply Unified Audit Log Extra." -sev Info
+try {
+    $DehydratedTenant = (New-ExoRequest -tenantid $Tenant -cmdlet "Get-OrganizationConfig").IsDehydrated
+    if ($DehydratedTenant) {
+        New-ExoRequest -tenantid $Tenant -cmdlet "Enable-OrganizationCustomization"
+    }
+    $users = New-GraphGetRequest -uri "https://graph.microsoft.com/beta/users/?`$top=999&`$select=id,userPrincipalName,assignedLicenses" -Tenantid $tenantfilter
+    $AuditLogAgeLimit = 365
+    (New-ExoRequest -tenantid $TenantFilter -cmdlet "Get-mailbox") |Select UserPrincipalName|%{  (New-ExoRequest -tenantid $TenantFilter -cmdlet "Set-Mailbox" -cmdParams @{Identity = $_.userprincipalname; AuditEnabled = $true; AuditLogAgeLimit = $AuditLogAgeLimit; `
+    AuditAdmin = @{Add="Copy","Create","FolderBind","HardDelete","Move","MoveToDeletedItems","SendAs","SendOnBehalf","SoftDelete","Update","UpdateFolderPermissions","UpdateInboxRules","UpdateCalendarDelegation"}; `
+    AuditDelegate = @{Add="Create","FolderBind","HardDelete","Move","MoveToDeletedItems","SendAs","SendOnBehalf","SoftDelete","Update","UpdateFolderPermissions","UpdateInboxRules"};  `
+    AuditOwner = @{Add="Create","HardDelete","Move","Mailboxlogin","MoveToDeletedItems","SoftDelete","Update","UpdateFolderPermissions","UpdateInboxRules","UpdateCalendarDelegation"}}})
+}
+catch {
+    $ErrorMessage = Get-NormalizedError -Message $_.Exception.Message
+    Write-LogMessage -API "Standards" -tenant $tenant -message "Failed to apply Unified Audit Log Extra. Error: $ErrorMessage" -sev Error
+}
